0aff89176cf44deb589dc70f1d2ccd6fa22e5f48
Denis Beqiraj <denisbeqi2312@gmail.com>
2020-01-31 12:37:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -90,7 +90,7 @@ int main()
 
 	// glfw window creation
 	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Snake", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
